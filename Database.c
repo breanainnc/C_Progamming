aa1bf107864828161f4b163ddd23218961265aa4
breanainnc <breanainnc@live.ie>
2023-02-09 11:42:53 -0800

    Update Database.c

--- Database.c
+++ Database.c
@@ -10,6 +10,7 @@
 struct UserInfo user;
 int UserCount = 0;
 int TableCount = 0;
+char* transferTarget = "0";
 
 //callback used to print info from SQL query result 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
@@ -25,6 +26,7 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
 static int InputCallback(void *NotUsed, int argc, char **argv, char **azColName) {
         user.ID = atoi(argv[0]);
         strcat(user.fullname, argv[1]);
+        user.Balance = atoi(argv[2]);
         return 0;
 }
 
@@ -38,6 +40,16 @@ static int UsrCountCallback(void *NotUsed, int argc, char **argv, char **azColNa
 //callback used to reveal if table exists
 static int TableExistsCallback(void *NotUsed, int argc, char **argv, char **azColName) {
         TableCount = atoi(argv[0]);
+        return 0;
+}
+
+//callback used to reveal if user exists
+static int transferCheckCallback(void *NotUsed, int argc, char **argv, char **azColName) {
+        user.ID = atoi(argv[0]);
+        user.Balance = atoi(argv[1]);
+        return 0;
+}
+static int updateCallback(void *NotUsed, int argc, char **argv, char **azColName) {
         return 0;
 }
 
@@ -56,7 +68,7 @@ int CreateUser(char* username, char* password, char* fullname, char* address, in
         sqlite3 *db;
         int rc;
         char *err = NULL;
-        
+
         rc = sqlite3_open("test.db", &db);
 
         if (rc)
@@ -149,7 +161,7 @@ int checkInput(char* usrname, char* pswrd)
                 }
         }
 
-        char sql[150] = "SELECT ID, FULLNAME FROM USERDB WHERE USERNAME='";
+        char sql[150] = "SELECT ID, FULLNAME, BALANCE FROM USERDB WHERE USERNAME='";
         char sqlMiddle[] = "' AND PASSWORD='";
         char sqlEnd[] = "';";
         strcat(sql, user.username);
@@ -162,7 +174,7 @@ int checkInput(char* usrname, char* pswrd)
 
         if( rc != SQLITE_OK ) {
                 fprintf(stderr, "SQL error: %s\n", err);
-                sqlite3_free(err);
+                 sqlite3_free(err);
                 return 1;
 
         } else {
@@ -220,8 +232,7 @@ int createTable()
 //Checks if USERDB exists and runs create function it is not created
 int checkTableExists(){
         sqlite3 *db;
-        int rc;
-        char *err = NULL;
+        int rc;    char *err = NULL;
 
         rc = sqlite3_open("test.db", &db);
 
@@ -249,3 +260,67 @@ int checkTableExists(){
         printf(": System Ready\n");
         return 0;
 }
+
+//checks if transfer is possible and updates tables if so
+int checkLegalTransfer(char* otherUser, int amount)
+{
+        sqlite3 *db;
+        int rc;
+        char *err = NULL;
+
+        rc = sqlite3_open("test.db", &db);
+
+        if (rc)
+        {
+                fprintf(stderr, "Cant open DB\n");
+                return(1);
+        }
+        else
+        {
+                fprintf(stderr, "Opened DB\n");
+        }
+
+
+        if(amount > getBalance())
+        {
+                printf("Insufficent FUNDS!\n");
+                return 1;
+        }
+
+        int id = user.ID;
+        int balance = user.Balance;
+        user.ID = 0;
+        char sql[100] = "SELECT ID, BALANCE FROM USERDB WHERE USERNAME='";
+                strcat(sql, otherUser);
+        strcat(sql, "';");
+        rc = sqlite3_exec(db, sql, transferCheckCallback, 0, &err);
+        if(user.ID == 0)
+        {
+                printf("USER NOT FOUND\n");
+                user.ID = id;
+                return 1;
+        }
+
+        int targetBalance = amount + user.Balance;
+        char sqlUpdateTarget[100] = "UPDATE USERDB SET BALANCE=";
+        sprintf(sqlUpdateTarget + strlen(sqlUpdateTarget), "%d", targetBalance );
+        strcat(sqlUpdateTarget, " WHERE ID=");
+        sprintf(sqlUpdateTarget + strlen(sqlUpdateTarget), "%d", user.ID);
+        strcat(sqlUpdateTarget, ";");
+        rc = sqlite3_exec(db, sqlUpdateTarget, updateCallback, 0, &err);
+
+        user.ID = id;
+        user.Balance = balance - amount;
+
+        char sqlUpdate[100] = "UPDATE USERDB SET BALANCE=";
+        sprintf(sqlUpdate + strlen(sqlUpdate), "%d", user.Balance );
+        strcat(sqlUpdate, " WHERE ID=");
+        sprintf(sqlUpdate + strlen(sqlUpdate), "%d", user.ID);
+        strcat(sqlUpdate, ";");
+        rc = sqlite3_exec(db, sqlUpdate, updateCallback, 0, &err);
+
+        sqlite3_close(db);
+        return 0;
+}
+
+        
